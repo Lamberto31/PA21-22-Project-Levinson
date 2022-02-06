a89f7a096a2a0ca6a715ad599fc9d8ba1406cf15
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2022-02-06 20:09:35 +0100

    Migliorato easy_check.py con inserimento input come vettore

--- easy_check.py
+++ easy_check.py
@@ -1,6 +1,9 @@
 import numpy as np
-m_list = [[1,2,4,6],[3,1,2,4],[5,3,1,2],[7,5,3,1]]
-B = np.array([1,2,3,4])
+t=[4,2,1,3,5]
+B = np.array([1,2,3])
+
+m_list = [[t[2],t[1],t[0]],[t[3],t[2],t[1]],[t[4],t[3],t[2]]]
 A = np.array(m_list)
+print(A)
 X = np.linalg.inv(A).dot(B)
 print(X)
