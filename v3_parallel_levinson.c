2b07744a5fa381c20207a87a1b2c1244802baf46
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2022-02-11 15:53:13 +0100

    Creato nuovo file v3_parallel_levinson.c

---
+++ v3_parallel_levinson.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <mpi.h>
+
+#define MAX_VALUE 10
+#define LOOP_COUNT 1
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
