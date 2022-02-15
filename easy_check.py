a659cf0aec052ba7dbfce1b18d2c53dd739a4162
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2022-02-15 17:56:34 +0100

    Fix easy_check.py per convertire vettore toeplitz in matrice

--- easy_check.py
+++ easy_check.py
@@ -1,8 +1,15 @@
 import numpy as np
-t=[4,2,1,3,5]
-B = np.array([1,2,3])
+#t = [4,2,1,3,5]
+t = [6,4,2,1,3,5,7]
+B = np.array([1,2,3,4])
+n = 4
+m_list = []
+for i in range(n):
+    rows = []
+    for j in range(n):
+        rows.append(t[i-j+n-1])
+    m_list.append(rows)
 
-m_list = [[t[2],t[1],t[0]],[t[3],t[2],t[1]],[t[4],t[3],t[2]]]
 A = np.array(m_list)
 print(A)
 X = np.linalg.inv(A).dot(B)
