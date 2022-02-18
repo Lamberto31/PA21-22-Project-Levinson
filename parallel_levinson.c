9df42f480977660fdcd957c0197efa8247c2228d
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2022-02-18 14:31:04 +0100

    Modificato calcolo operazioni per iterazione in divide_work

--- parallel_levinson.c
+++ parallel_levinson.c
@@ -265,8 +265,8 @@ void create_resized_interleaved_vector_datatype(long n, int stride, MPI_Datatype
 }
 
 void divide_work(long it, int id, int p, long *ops_errors, long *ops_update, int *ring_size) {
-  *ops_errors += ((it-id-1)%p == 0);
-  *ops_update += ((it-id)%p == 0);
+  *ops_errors = it/p + (it % p > id);
+  *ops_update = (it+1)/p + ((it+1) % p > id);
   if (it+1 < p) {
     *ring_size = it+1;
   } else {
@@ -316,10 +316,6 @@ void parallel_levinson(int id, int p, long n, double *t, double *y, long v_size,
 
   //Vectors Update
   double f_temp;
-
-  //Operations division
-  ops_errors = 0;
-  ops_update = (id == 0);
 
   for (long it = 1; it < n; it++) {
     divide_work(it, id, p, &ops_errors, &ops_update, &ring_size);
